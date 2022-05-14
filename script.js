6ad43c938f85778f23ea9242785e0fdf0400ff5e
Nimo Abdi <nimoabdi318@gmail.com>
2022-05-14 17:32:34 -0500

    made first name work

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('ready to start'); }
+
+    $(document).on('click', '#submit-Button', onSubmit);
+
+    function onSubmit() {
+        let firstName = $('.first-name').val();
+        console.log('first name', firstName);
+
+
+
+
+
+    }
+    
