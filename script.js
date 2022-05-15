42e08bec0c14ce4f5462b177cf39c12eedf072c6
Nimo Abdi <nimoabdi318@gmail.com>
2022-05-14 20:11:57 -0500

    created append and moved things around

--- script.js
+++ script.js
@@ -8,21 +8,45 @@ function onReady() {
     $(document).on('click', '#submit-Button', onSubmit);
 
     function onSubmit() {
-        let firstName = $('.first-name').val();
-        console.log('first name', firstName);
+        let FirstName = $('.first-name').val();
+        console.log('first name', FirstName);
 
         let lastName = $('.last-name').val();
         console.log('last name', lastName);
 
         let ID = $('.id-number').val();
-        console.log('id-number', ID);
+        console.log('id number', ID);
 
         let Title = $('.job-title').val();
         console.log('job title', Title);
 
         let annualSalary = $('.annual-salary').val();
-        console.log('annual-salary', annualSalary);
+        console.log('annual salary', annualSalary);
 
     }
 
+    $('#employeeTable').append(`
+    <tr>
+       <td>${FirstName}</td>
+       <td>${lastName}</td>
+       <td>${ID}</td>
+       <td>${Title}</td>
+       <td>$annualSalary ${annualSalary} </td>
+    
+    
+    
+    `);
+
+
+
+
+  // $('#employeeTable').append(`
+      //  <tr>
+        //    <td>${firstname}</td>
+         //   <td>${lastmame}</td>
+           // <td>${ID}</td>
+           // <td>${annualsalary}</td>
+          //  </td>
+       // </tr>    
+       // `);
     
