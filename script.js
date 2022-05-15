8c803f92b62c7f277dd53d04e55e5615da6fc14a
Nimo Abdi <nimoabdi318@gmail.com>
2022-05-15 17:19:32 -0500

    delete button is deleting the whole thing for some reason

--- script.js
+++ script.js
@@ -3,57 +3,68 @@ console.log('In script.js');
 $(document).ready(onReady);
 
 function onReady() {
-    console.log('ready to start'); }
-
-    $(document).on('click', '#submitButton', addEmployee);
-
-    function addEmployee() {
-        let FirstName = $('#firstName').val();
-        console.log('first name', FirstName);
-
-        let lastName = $('#lastName').val();
-        console.log('last name', lastName);
-
-        let ID = $('#idNumber').val();
-        console.log('id number', ID);
-
-        let Title = $('#jobTitle').val();
-        console.log('job title', Title);
-
-        let annualSalary = $('#annualSalary').val();
-        console.log('annual salary', annualSalary);
-
+  console.log('ready to start'); }
+  
+  $(document).on('click', '#submitButton', addEmployee);
+  $(document).on('click', '.deleteButton', onDelete);
+  
+  function addEmployee() {
+    let FirstName = $('#firstName').val();
+    console.log('first name', FirstName);
+    
+    let lastName = $('#lastName').val();
+    console.log('last name', lastName);
+    
+    let ID = $('#idNumber').val();
+    console.log('id number', ID);
+    
+    let Title = $('#jobTitle').val();
+    console.log('job title', Title);
+    
+    let annualSalary = $('#annualSalary').val();
+    console.log('annual salary', annualSalary);
+    
+  }
+  // testing to see if i need a function before the append
+  function inTable() {
+    $('.employeeTable').empty();
+    for(let list of employee) {
+      $('.employeeTable').append(`
+      <tr>
+      <td>${list.FirstName}</td>
+      <td>${list.lastName}</td>
+      <td>${list.ID}</td>
+      <td>${list.Title}</td>
+      <td>$annualSalary ${annualSalary} </td>
+      <td> <input type="button" class="deleteButton" value="Delete"</td>
+      
+      `);
     }
-
-    function inTable() {
-      $('.employeeTable').empty();
-      for(let list of employee) {
-        $('.employeeTable').append(`
-    <tr>
-       <td>${list.FirstName}</td>
-       <td>${list.lastName}</td>
-       <td>${list.ID}</td>
-       <td>${list.Title}</td>
-       <td>$annualSalary ${annualSalary} </td>
-    
-    
-    
-    `);
-      }
-    }
-
-    
-
-
-
-
+  }
+  
+  function onDelete() {
+    let  tr= $(this).parent().parent();
+    let employee= $(this)
+    .parent()
+    .siblings()
+    .first()
+    .text();
+    alert('Employee deleted 😓 ${employee}.', employee);
+    tr.remove();
+  }
+  
+  
+  
+  
+  
+  
   // $('#employeeTable').append(`
-      //  <tr>
-        //    <td>${firstname}</td>
-         //   <td>${lastmame}</td>
-           // <td>${ID}</td>
-           // <td>${annualsalary}</td>
-          //  </td>
-       // </tr>    
-       // `);
-    
+  //  <tr>
+  //    <td>${firstname}</td>
+  //   <td>${lastmame}</td>
+  // <td>${ID}</td>
+  // <td>${annualsalary}</td>
+  //  </td>
+  // </tr>    
+  // `);
+  
