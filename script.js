af9cd3685b1a4cc8cc27e9c04005b40a3d815cb8
Nimo Abdi <nimoabdi318@gmail.com>
2022-05-15 23:27:47 -0500

    finally figured out i had the bracket in the wrong place

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log('In script.js');
 
 $(document).ready(onReady);
 
-let employees = [];
+
 
 function onReady() {
   console.log('ready to start'); }
@@ -10,6 +10,7 @@ function onReady() {
   $(document).on('click', '#submitButton', addEmployee);
   $(document).on('click', '.deleteButton', onDelete);
   
+  let employees = [];
   function addEmployee() {
    
     let firstNameInputs = $('#firstNameInputs').val();
@@ -29,50 +30,46 @@ function onReady() {
     }
     // pushing employees into the array
     employees.push(newEmployees);
-  }
+  
 
 
 
   // testing to see if i need a function before the append
- 
-      $('.employeeTable').append(`
-      <tr>
-        <td>${firstNameInputs}</td>
-        <td>${lastNameInputs}</td>
-        <td>${employeeIDNumberInputs}</td>
-        <td>${jobTitleInputs}</td>
-        <td>${ annualSalaryInputs} </td>
-        <td> 
-          <button class="deleteButton">Delete</button>
-        </td>  
-      </tr>  
-      `);
+  $('#employeeTable').append(`
+  <tr>
+      <td>${firstNameInputs}</td>
+      <td>${lastNameInputs}</td>
+      <td>${employeeIDNumberInputs}</td>
+      <td>${jobTitleInputs}</td>
+      <td>${annualSalaryInputs}</td>
+      <td><button class="deleteButton">Delete</button></td>
+  </tr>`);
 
-
-      $("#firstName").val('');
-      $("#lastName").val('');
-      $("#idNumber").val('');
-      $("#jobTitle").val('');
-      $("#annualSalary").val('');
+    // emptying array
+      $("#firstNameInputs").val('');
+      $("#lastNameInputs").val('');
+      $("#idNumberInputs").val('');
+      $("#jobTitleInputs").val('');
+      $("#annualSalaryInputs").val('');
     
-  
+  }
 
   
   function onDelete() {
     let  tr= $(this).parent().parent();
-    let employees= $(this)
+    let employee= $(this)
     .parent()
     .siblings()
     .first()
     .text();
-    alert('Employee deleted 😓 ', employees);
+    alert('Employee deleted 😓 ${employee}.', employee);
     tr.remove();
   }
-  
+
 
 function calculateSalary(employee) {
   let employeeSalary = 0;
-  for (let employeeSalary of employee) {
+  for (let employeeSalary of employees) {
     employeeSalary += employeeSalary.annualSalary;
   }
   employeeSalary /12;
@@ -80,9 +77,8 @@ function calculateSalary(employee) {
     $('totalMonthlyCost').css('background-color', 'red');
   }
 }
+  
 
-
-  // empty array to loop through the empoyees salary
-  //annualSalary(); {
+ 
 
 
