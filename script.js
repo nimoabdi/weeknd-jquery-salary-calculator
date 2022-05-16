9222c4929f399379a69158fbacad86c05c056b6d
Nimo Abdi <nimoabdi318@gmail.com>
2022-05-16 00:07:42 -0500

    css changes

--- script.js
+++ script.js
@@ -12,73 +12,72 @@ function onReady() {
   
   let employees = [];
   function addEmployee() {
-   
+    
     let firstNameInputs = $('#firstNameInputs').val();
     let lastNameInputs = $('#lastNameInputs').val();
     let employeeIDNumberInputs = $('#idNumberInputs').val();
     let jobTitleInputs = $('#jobTitleInputs').val();
     let annualSalaryInputs = $('#salaryInputs').val();
-  
-   
+    
+    
     let newEmployees = {
-        firstName: firstNameInputs,
-        lastName: lastNameInputs,
-        employeeID: employeeIDNumberInputs,
-        employeeTitle: jobTitleInputs,
-        annualSalary: annualSalaryInputs
-
+      firstName: firstNameInputs,
+      lastName: lastNameInputs,
+      employeeID: employeeIDNumberInputs,
+      employeeTitle: jobTitleInputs,
+      annualSalary: annualSalaryInputs
+      
     }
     // pushing employees into the array
     employees.push(newEmployees);
-  
-
-
-
-  // testing to see if i need a function before the append
-  $('#employeeList').append(`
-  <tr>
+    
+    
+    
+    
+    // testing to see if i need a function before the append
+    $('#employeeList').append(`
+    <tr>
       <td>${firstNameInputs}</td>
       <td>${lastNameInputs}</td>
       <td>${employeeIDNumberInputs}</td>
       <td>${jobTitleInputs}</td>
       <td>${annualSalaryInputs}</td>
-      <td><button class="deleteButton">Delete</button></td>
-  </tr>`);
-
+      <td>
+      <button class="deleteButton">Delete</button>
+      </td>
+    </tr>`);
+    
     // emptying array
-      $("#firstNameInputs").val('');
-      $("#lastNameInputs").val('');
-      $("#idNumberInputs").val('');
-      $("#jobTitleInputs").val('');
-      $("#annualSalaryInputs").val('');
+    $("#firstNameInputs").val('');
+    $("#lastNameInputs").val('');
+    $("#idNumberInputs").val('');
+    $("#jobTitleInputs").val('');
+    $("#annualSalaryInputs").val('');
     
   }
-
+  
   // delete button is showing up as undefined so i need to figure that out
   function onDelete() {
-    let  tr= $(this).parent().parent();
-    let employees= $(this)
-    .parent()
-    .siblings()
-    .first()
-    .text();
-    alert('Employee deleted 😓 ', employees);
+    let tr = $(this).parents('tr');
     tr.remove();
+    alert('Employee deleted 😓 ', employees);
+    
   }
-
-
-function calculateSalary(employees) {
-  let employeeSalary = 0;
-  for (let employeeSalary of employees) {
-    employeeSalary += employeeSalary.annualSalary;
-  }
-  employeeSalary /12;
-  if (employeeSalary /12 >= 20000) {
-    $('totalMonthlyCost').css('background-color', 'red');
-  }
-}
   
-
- 
-
-
+  
+  function calculateSalary() {
+    let employeeSalary = 0;
+    for (let employeeSalary of employees) {
+      employeeSalary += employeeSalary.annualSalary;
+    }
+    employeeSalary /12;
+    if (employeeSalary /12 >= 20000) {
+      $('totalMonthlyCost').css('background-color', 'red');
+    }
+  }
+  
+  
+  
+  
+  
+  
