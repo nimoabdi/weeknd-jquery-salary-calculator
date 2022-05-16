8ce52ab73d3e0c0a0c185a6f2e3b183991d1d00a
Nimo Abdi <nimoabdi318@gmail.com>
2022-05-15 22:58:28 -0500

    created an object with arrays since the way i was doing it was confusing

--- script.js
+++ script.js
@@ -2,77 +2,87 @@ console.log('In script.js');
 
 $(document).ready(onReady);
 
+let employees = [];
+
 function onReady() {
   console.log('ready to start'); }
   
   $(document).on('click', '#submitButton', addEmployee);
-  $(document).on('click', '#deleteButton', onDelete);
+  $(document).on('click', '.deleteButton', onDelete);
   
   function addEmployee() {
-    let FirstName = $('#firstName').val();
-    console.log('first name', FirstName);
-    
-    let lastName = $('#lastName').val();
-    console.log('last name', lastName);
-    
-    let ID = $('#idNumber').val();
-    console.log('id number', ID);
-    
-    let Title = $('#jobTitle').val();
-    console.log('job title', Title);
-    
-    let annualSalary = $('#annualSalary').val();
-    console.log('annual salary', annualSalary);
-    
+   
+    let firstNameInputs = $('#firstNameInputs').val();
+    let lastNameInputs = $('#lastNameInputs').val();
+    let employeeIDNumberInputs = $('#idNumberInputs').val();
+    let jobTitleInputs = $('#jobTitleInputs').val();
+    let annualSalaryInputs = $('#salaryInputs').val();
+  
+   
+    let newEmployees = {
+        firstName: firstNameInputs,
+        lastName: lastNameInputs,
+        employeeID: employeeIDNumberInputs,
+        employeeTitle: jobTitleInputs,
+        annualSalary: annualSalaryInputs
+
+    }
+    // pushing employees into the array
+    employees.push(newEmployees);
   }
+
+
+
   // testing to see if i need a function before the append
-  function inTable() {
-    $('.employeeTable').empty();
-    for(let list of employee) {
+ 
       $('.employeeTable').append(`
       <tr>
-      <td>${list.FirstName}</td>
-      <td>${list.lastName}</td>
-      <td>${list.ID}</td>
-      <td>${list.Title}</td>
-      <td>$annualSalary ${annualSalary} </td>
-      <td> <input type="button" class="deleteButton" value="Delete"</td>
-      
+        <td>${firstNameInputs}</td>
+        <td>${lastNameInputs}</td>
+        <td>${employeeIDNumberInputs}</td>
+        <td>${jobTitleInputs}</td>
+        <td>${ annualSalaryInputs} </td>
+        <td> 
+          <button class="deleteButton">Delete</button>
+        </td>  
+      </tr>  
       `);
-    }
-  }
+
+
+      $("#firstName").val('');
+      $("#lastName").val('');
+      $("#idNumber").val('');
+      $("#jobTitle").val('');
+      $("#annualSalary").val('');
+    
+  
+
   
   function onDelete() {
     let  tr= $(this).parent().parent();
-    let employee= $(this)
+    let employees= $(this)
     .parent()
     .siblings()
     .first()
     .text();
-    alert('Employee deleted 😓 ${employee}.', employee);
+    alert('Employee deleted 😓 ', employees);
     tr.remove();
   }
   
-  // empty array to loop through the empoyees salary
-  addSalary(); {
 
-  $("#firstName").val();
-  $("#lastName").val();
-  $("#idNumber").val();
-  $("#jobTitle").val();
-  $("#annualSalary").val();
+function calculateSalary(employee) {
+  let employeeSalary = 0;
+  for (let employeeSalary of employee) {
+    employeeSalary += employeeSalary.annualSalary;
+  }
+  employeeSalary /12;
+  if (employeeSalary > 20000) {
+    $('totalMonthlyCost').css('background-color', 'red');
+  }
 }
-  
-  
-  
-  
-  // $('#employeeTable').append(`
-  //  <tr>
-  //    <td>${firstname}</td>
-  //   <td>${lastmame}</td>
-  // <td>${ID}</td>
-  // <td>${annualsalary}</td>
-  //  </td>
-  // </tr>    
-  // `);
-  
+
+
+  // empty array to loop through the empoyees salary
+  //annualSalary(); {
+
+
